bb11029b42098ab2afa3c4d696e9948cdff622c0
David Guttman <david@davidguttman.com>
2017-12-27 18:15:41 -0800

    feat: replaced random announce phrases with standard message

--- server/announce.js
+++ server/announce.js
@@ -1,16 +1,6 @@
 var discord = require('./discord')
 var followStream = require('./twitch')
 
-var suffixes = [
-  'say hi 👋',
-  'are they working on anything interesting?',
-  'where do they live?',
-  'how did they find the channel?',
-  'what games are they currently playing?',
-  'what\'s the story behind their nickname?',
-  'do they have any favorite gdq runs?'
-]
-
 module.exports = function (stream, channel) {
   var sendMessage = discord(channel)
 
@@ -19,7 +9,7 @@ module.exports = function (stream, channel) {
     .on('data', function (data) {
       console.log('data', data)
       if (data.doesFollow) return
-      var suffix = suffixes[Math.floor(Math.random() * suffixes.length)]
-      sendMessage(`**${data.fromUser}** is probably new, ${suffix}`)
+
+      sendMessage(`Possibly New Viewer: **${data.fromUser}**`)
     })
 }
